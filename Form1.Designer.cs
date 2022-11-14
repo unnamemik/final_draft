c4fb4764e24a88f0b2612f7f5194efbf3682bf57
Mikael <unnamemik@gmail.com>
2022-11-14 03:47:38 +0300

    Main method create.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,16 +32,26 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,17 +64,19 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(828, 256);
+            this.tabControl1.Size = new System.Drawing.Size(828, 342);
             this.tabControl1.TabIndex = 0;
+            this.tabControl1.Selected += new System.Windows.Forms.TabControlEventHandler(this.tabControl1_Selected);
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(820, 223);
+            this.tabPage1.Size = new System.Drawing.Size(820, 309);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Ввод вручную";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -87,27 +99,111 @@
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox1.Size = new System.Drawing.Size(800, 200);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Введите текст и нажмите Enter";
+            this.textBox1.Text = "Введите текст";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(281, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.textBox5);
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Controls.Add(this.button2);
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(820, 223);
+            this.tabPage2.Size = new System.Drawing.Size(820, 309);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Случайный ввод";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(94, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(268, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Количество элементов в массиве";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Верхний диапазон значений";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(477, 87);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(168, 26);
+            this.textBox5.TabIndex = 3;
+            this.textBox5.Text = "0";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(477, 34);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(168, 26);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "0";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button2.Location = new System.Drawing.Point(243, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(316, 88);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Сгенерировать массив";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.button3);
             this.tabPage3.Location = new System.Drawing.Point(4, 29);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(820, 223);
+            this.tabPage3.Size = new System.Drawing.Size(820, 309);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Ввод из файла";
             this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(276, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(215, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Выберите файл с текстом.";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(250, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(268, 91);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Открыть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // tabPage4
             // 
@@ -116,7 +212,7 @@
             this.tabPage4.Location = new System.Drawing.Point(4, 29);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(820, 223);
+            this.tabPage4.Size = new System.Drawing.Size(820, 309);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "О программе";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -124,7 +220,7 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(464, 630);
+            this.linkLabel1.Location = new System.Drawing.Point(323, 147);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(142, 20);
             this.linkLabel1.TabIndex = 1;
@@ -142,20 +238,9 @@
     "массива. \r\nПервоначальный массив задается одним из трех способов - вручную, случ" +
     "айно и из файла с \r\nрасширением .txt.";
             // 
-            // button1
-            // 
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(278, 505);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(234, 60);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Найти";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(16, 274);
+            this.textBox2.Location = new System.Drawing.Point(12, 372);
             this.textBox2.MaximumSize = new System.Drawing.Size(800, 200);
             this.textBox2.MinimumSize = new System.Drawing.Size(800, 200);
             this.textBox2.Multiline = true;
@@ -163,12 +248,12 @@
             this.textBox2.ReadOnly = true;
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox2.Size = new System.Drawing.Size(800, 200);
-            this.textBox2.TabIndex = 1;
+            this.textBox2.TabIndex = 3;
             this.textBox2.Text = "Исходный массив:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(16, 601);
+            this.textBox3.Location = new System.Drawing.Point(12, 588);
             this.textBox3.MaximumSize = new System.Drawing.Size(800, 200);
             this.textBox3.MinimumSize = new System.Drawing.Size(800, 200);
             this.textBox3.Multiline = true;
@@ -176,23 +261,32 @@
             this.textBox3.ReadOnly = true;
             this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox3.Size = new System.Drawing.Size(800, 200);
-            this.textBox3.TabIndex = 1;
+            this.textBox3.TabIndex = 4;
             this.textBox3.Text = "Результат:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 816);
+            this.ClientSize = new System.Drawing.Size(828, 814);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(850, 870);
+            this.MinimumSize = new System.Drawing.Size(850, 870);
             this.Name = "Form1";
             this.Text = "Поиск элементов в массиве";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.tabPage4.ResumeLayout(false);
             this.tabPage4.PerformLayout();
             this.ResumeLayout(false);
@@ -214,6 +308,14 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -23,9 +23,86 @@ namespace final_draft
             System.Diagnostics.Process.Start("https://github.com/unnamemik/final_draft");
         }
 
+
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox2.Text = textBox1.Text;
+            string[] input_array = textBox1.Text.Split(' ');
+            textBox2.Text = "[" + String.Join(", ", input_array) + "]";
+            int output_arrsize = 0;
+            string[] output_array = new string[output_arrsize];
+
+            for (int i = 0; i < input_array.Length; i++)
+            {
+                if (input_array[i].Length <= 3)
+                {                    
+                    Array.Resize(ref output_array, output_arrsize+1);
+                    output_array[output_arrsize] = input_array[i];
+                    output_arrsize++;
+                }
+            }
+            textBox3.Text = "[" + String.Join(", ", output_array) + "]";
+        }
+
+        private void tabControl1_Selected(object sender, TabControlEventArgs e)
+        {
+            textBox2.Text = "Исходный массив:";
+            textBox3.Text = "Результат";
+            textBox4.Text = "0";
+            textBox5.Text = "0";
+        }        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int diapazon = int.Parse(textBox4.Text);
+            int rand_Array_size = int.Parse(textBox5.Text);
+            int[] rand_Array = new int[rand_Array_size];
+            Random rand = new Random();
+
+            for (int x = 0; x < rand_Array.Length; x++)
+            {
+                rand_Array[x] = rand.Next(diapazon);
+            }
+            textBox2.Text = "[" + String.Join(", ", rand_Array) + "]";
+            int output_arrsize = 0;
+            int[] output_array = new int[output_arrsize];
+
+            for (int i = 0; i < rand_Array.Length; i++)
+            {
+                if (rand_Array[i] <= 999)
+                {
+                    Array.Resize(ref output_array, output_arrsize + 1);
+                    output_array[output_arrsize] = rand_Array[i];
+                    output_arrsize++;
+                }
+            }
+            textBox3.Text = "[" + String.Join(", ", output_array) + "]";
+        }  
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+            string fileText = System.IO.File.ReadAllText(filename);
+            textBox2.Text = fileText;
+
+            string[] input_array = textBox2.Text.Split(' ');
+            textBox2.Text = "[" + String.Join(", ", input_array) + "]";
+            int output_arrsize = 0;
+            string[] output_array = new string[output_arrsize];
+
+            for (int i = 0; i < input_array.Length; i++)
+            {
+                if (input_array[i].Length <= 3)
+                {
+
+                    Array.Resize(ref output_array, output_arrsize + 1);
+                    output_array[output_arrsize] = input_array[i];
+                    output_arrsize++;
+                }
+            }
+            textBox3.Text = "[" + String.Join(", ", output_array) + "]";
         }
     }
 }
